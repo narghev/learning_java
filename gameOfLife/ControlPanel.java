988332774e6b1113e51eecc14d0d36e7f10be6ba
Narek Ghevandiani <n.ghevandiani@gmail.com>
2017-11-03 23:10:25 +0400

    reset click handler

--- gameOfLife/ControlPanel.java
+++ gameOfLife/ControlPanel.java
@@ -18,7 +18,7 @@ public class ControlPanel extends JPanel {
 
         controls[0] = new JButton("Reset");
         controls[1] = new JButton("Random");
-        controls[2] = new JButton("Select");
+        controls[2] = new JButton("Step");
 
         for (JButton button : controls)
             add(button);
--- gameOfLife/GameOfLife.java
+++ gameOfLife/GameOfLife.java
@@ -9,7 +9,7 @@ import java.awt.event.ActionListener;
 import java.awt.FlowLayout;
 import java.awt.GridLayout;
 
-public class GameOfLife extends JFrame {
+public class GameOfLife extends JFrame implements ActionListener {
 
     private LifePanel ButtonPanel = new LifePanel(5);
     private ControlPanel ControlPanel = new ControlPanel();
@@ -18,11 +18,30 @@ public class GameOfLife extends JFrame {
         super();
         setLayout(new GridLayout());
 
+        for (JButton controlButton : ControlPanel.controls)
+            controlButton.addActionListener(this);
+
         add(ButtonPanel);
         add(ControlPanel);
 
         setSize(800, 300);
         setResizable(true);
         setVisible(true);
+    }
+
+    public void actionPerformed(ActionEvent e){
+        JButton clicketButton = (JButton)e.getSource();
+        String clickedButtonText = clicketButton.getText();
+        
+        switch (clickedButtonText){
+            case "Reset":
+                for (LifeButton[] cellRow : ButtonPanel.cells)
+                    for (LifeButton cell : cellRow)
+                        cell.setAlive(false);
+                break;
+                
+            default:
+                break;
+        }
     }
 }
